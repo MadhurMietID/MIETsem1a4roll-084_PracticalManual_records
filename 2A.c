766213c79c5d97101ce92ee74eb72126b385f27e
MadhurMietID <146841949+MadhurMietID@users.noreply.github.com>
2023-11-18 13:02:11 +0530

    Create 2A.c

---
+++ 2A.c
@@ -0,0 +1,14 @@
+//to perform arthemetic operation 
+#include<stdio.h>
+#include<math.h>
+void main() {
+    int a,b;
+    printf("enter the number");
+    scanf("%d%d",&a,&b);
+    int c = a + b;
+    int d = a - b;
+    int e = a * b;
+    printf("addition=%d\n",c);
+    printf("subtraction=%d\n",d);
+    printf("multiplication=%d\n",e);        
+}
