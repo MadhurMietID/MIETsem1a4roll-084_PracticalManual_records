174ae5fc0414973f2b5115dd47a966e707960b6f
MadhurMietID <146841949+MadhurMietID@users.noreply.github.com>
2023-11-19 19:52:59 +0530

    Create 3A.c

---
+++ 3A.c
@@ -0,0 +1,17 @@
+//C program to find if a number is odd or even.
+
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    if(n%2 == 0)
+    {
+        printf("%d is even",n);
+    }
+    else{
+        printf("%d is odd",n);
+    }
+    return 0;
+}
