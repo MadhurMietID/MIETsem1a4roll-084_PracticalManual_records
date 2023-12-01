609b79c8e207c480211ac61afdba3125c784af2f
MadhurMietID <146841949+MadhurMietID@users.noreply.github.com>
2023-12-01 11:01:53 +0530

    Update 8A.c

--- 8A.c
+++ 8A.c
@@ -1,1 +1,19 @@
-
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char str1[20],str2[20],str3[20];
+    int res;
+    printf("enter the string 1\n");
+    scanf("%s",str1);
+    printf("enter the string 2\n");
+    scanf("%s",str2);
+    printf("concatenation string is:%s\n",strcat(str1,str2));
+    res=strcmp(str1,str2);
+    printf("compare string is:%d\n",res);
+    strcpy(str3,str1);
+    printf("The first string is:%s\n",str1);
+    printf("The third string is:%s\n",str3);
+    printf("The third string upper case is:%s\n",strupr(str3));
+    printf("The third string lower case is:%s",strlwr(str3));
+}
